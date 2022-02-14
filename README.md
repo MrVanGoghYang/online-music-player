70e72beb31e95a4b0f555d50375d315ebfb59b91
MrVanGoghYang <972676241@qq.com>
2022-02-14 23:26:12 +0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # online-music-player
+A agile project repository —— online-music-player.
---
+++ online-music-player-server/src/test/java/com/agile/onlinemusicplayer/OnlineMusicPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.onlinemusicplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMusicPlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
